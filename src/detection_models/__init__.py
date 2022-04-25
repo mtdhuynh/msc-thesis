eb7567b6ea63711abd4b61b48ae88e2f25ebf316
Tam Huynh <tamdavide.huynh@gmail.com>
2022-04-25 11:55:31 +0000

    Implement object detection model class for model loading

--- src/detection_models/__init__.py
+++ src/detection_models/__init__.py
@@ -1,5 +1,7 @@
+from detection_models.detection_models import ODModel
 from detection_models.model_utils import get_model
 
 __all__ = [
-    get_model
+    get_model,
+    ODModel
 ]
---
+++ src/detection_models/detection_models.py
@@ -0,0 +1,40 @@
+import torch
+
+import detection_models
+
+SUPPORTED_ARCHITECTURES = ['YOLOv3', 'YOLOv5']
+
+class ODModel():
+    """
+    Custom model for Object Detection (OD) tasks.
+    Given a model name (for architecture and backbone), it tries loading the model from
+    the model zoo and return it.
+    """
+    def __init__(self, specs, device):
+        """
+        Loads one of the models present in models.model_zoo according to the
+        arch name provided. 
+
+        Parameters:
+            specs (dict)            : config dict containing model architecture, backbone, etc.
+            device (torch.device)   : device on which to load the model.
+        """
+        # Double-check device is available
+        self.device = device if torch.cuda.is_available() else 'cpu'
+        
+        # Get specifications
+        self.model_arch = specs['arch']
+        self.pretrained = specs['pretrained']
+        self.model_backbone = specs['backbone'] # can also be the model version (e.g. yolov3-TINY)
+        self.num_classes = specs['num_classes']
+
+        # Load model from our model_zoo
+        try:
+            # Get the function from the package list
+            model_func = detection_models.__dict__[self.model_arch]
+        except Exception as e:
+            print(e)
+            print(f'Available object detection model architectures: {SUPPORTED_ARCHITECTURES}')
+
+        # Initialize the model (model will be returned already on selected device)
+        self.model = model_func(backbone=self.model_backbone, pretrained=self.pretrained, num_classes=self.num_classes, device=self.device)
