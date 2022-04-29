c9f007cbddc6e3884f4a18daa1d3e6f03176c85c
Tam Huynh <tamdavide.huynh@gmail.com>
2022-04-29 13:33:28 +0000

    Implement prepare_training() training config parsing function

--- src/utilities/training_utils.py
+++ src/utilities/training_utils.py
@@ -1,11 +1,12 @@
+from calendar import c
 import torch
 
-from detection_models.model_utils import get_model
-from data.dataset_utils import get_dataloader, get_dataset, get_transforms
-from losses.losses_utils import get_loss_fn
-from metrics.metrics_utils import get_metrics
-from optimizers.optimizers_utils import get_optimizer
-from lr_schedulers.lr_schedulers_utils import get_lr_scheduler
+from detection_models import get_model
+from data import get_dataloader, get_dataset, get_transforms
+from losses import get_loss_fn
+from metrics import get_metrics
+from optimizers import get_optimizer
+from lr_schedulers import get_lr_scheduler
 
 def prepare_training(config, device, tb_writer, logger):
     """
@@ -30,60 +31,72 @@ def prepare_training(config, device, tb_writer, logger):
         if logger:
             logger.info(f'Using selected device: {device}')
     except Exception as ex:
-        print(f'Specified device not available. Expected ["cpu", "cuda"], got {device}.')
-
+        # Select available device automatically
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
         if logger:
+            logger.info(f'Specified device not available. Expected ["cpu", "cuda"], got {device}.')
+
             logger.info(f'Using default device: {device}')
 
     ##### TRANSFORMS #####
     normalize = config['dataset']['transforms'].pop('normalize')
 
     transforms = {
-        'train': get_transforms(mode='train', specs=config['dataset']['transforms'], normalize=normalize, logger=logger),
-        'val': get_transforms(mode='val', specs=config['dataset']['transforms'], normalize=normalize, logger=logger)
+        'train': get_transforms(mode='train', params=config['dataset'], normalize=normalize, logger=logger),
+        'val': get_transforms(mode='val', params=config['dataset'], normalize=normalize, logger=logger)
     }
 
     ##### DATASETS #####
     datasets = {
-        'train': get_dataset(mode='train', transforms=transforms, fpath=config['dataset']['fpath'], cache=config['dataset']['cache'], tb_writer=tb_writer, logger=logger),
-        'val': get_dataset(mode='val', transforms=transforms, fpath=config['dataset']['fpath'], cache=config['dataset']['cache'], logger=logger)
+        'train': get_dataset(mode='train', transforms=transforms, fpath=config['dataset']['fpath'], bbox_format=config['dataset']['format'], cache=config['dataset']['cache'], tb_writer=tb_writer, logger=logger),
+        'val': get_dataset(mode='val', transforms=transforms, fpath=config['dataset']['fpath'], bbox_format=config['dataset']['format'], cache=config['dataset']['cache'], logger=logger)
     }
 
     ##### DATALOADERS #####
     dataloaders = {
-        'train': get_dataloader(mode='train', dataset=datasets['train'], specs=config['training'], logger=logger),
-        'val': get_dataloader(mode='val', dataset=datasets['val'], specs=config['training'], logger=logger)
+        'train': get_dataloader(mode='train', dataset=datasets['train'], params=config['training'], logger=logger),
+        'val': get_dataloader(mode='val', dataset=datasets['val'], params=config['training'], logger=logger)
     }
 
     ##### MODEL #####
     model = get_model(specs=config['model'], device=device, logger=logger)
 
     ##### OPTIMIZER #####
-    optimizer = get_optimizer(model=model, specs=config['training']['optimizer'], logger=logger)
+    optimizer = get_optimizer(model=model, logger=logger, **config['training']['optimizer'])
 
     ##### LR SCHEDULER #####
-    lr_scheduler = get_lr_scheduler(optimizer=optimizer, specs=config['training']['lr_scheduler'], logger=logger)
+    lr_scheduler = get_lr_scheduler(optimizer=optimizer, logger=logger, **config['training']['lr_scheduler'])
 
     ##### LOSS FUNCTION #####
-    loss_function = get_loss_fn(specs=config['training']['loss'], logger=logger)
+    # YOLOLoss needs also the model as input
+    if config['model']['arch'].startswith('YOLO'):
+        config['training']['loss']['model'] = model 
+
+    loss_function = get_loss_fn(logger=logger, **config['training']['loss'])
 
     ##### METRICS #####
-    metrics = get_metrics(specs=config['training']['metrics'], logger=logger)
+    metrics = get_metrics(device=device, logger=logger, **config['training']['mAP'])
 
     ##### EPOCHS & SAVING #####
     epochs = config['training']['num_epochs']
     early_stop = config['training']['early_stop']
 
+    save_best = config['checkpoints']['save_best'] # True or False
+    save_checkpoints = config['checkpoints']['save_checkpoints']
+    resume_checkpoint = config['checkpoints']['resume_checkpoint']
+    
     if logger:
         logger.info(f'Training for {epochs} epochs.')
-
         logger.info(f'Early stop after {early_stop} epochs without improvements in loss function minimization.')
-
-    save_best = config['checkpoints']['save_best'] # True or False
-    save_checkpoints = config['checkpoints']['save_checkpoints'] if isinstance(config['checkpoints']['save_checkpoints'], int) else False 
-    resume_checkpoint = config['checkpoints']['resume_checkpoint'] 
+        if save_best:
+            logger.info('Checkpointing every best model.')
+        if save_checkpoints:
+            logger.info('Checkpointing every 50 epochs.')
+        if resume_checkpoint:
+            logger.info('Resuming training from checkpoint: {resume_checkpoint}.')
+        else:
+            logger.info('Training from scratch.')
 
     ##### PREPARE TRAINING DICT #####
     training_dict = {
@@ -102,35 +115,3 @@ def prepare_training(config, device, tb_writer, logger):
     }
 
     return training_dict
-
-
-
-
-def train_one_epoch():
-    """
-    One loop of model training. 
-    """
-    pass
-
-def train(config, device, tb_writer, logger):
-    """"
-    Main training function. The function takes as input the configuration
-    file, the selected device (if available), the tensorboard writer and 
-    the logger objects.
-
-    Internally, it parses the config dict and loads all specified utilities
-    for training (dataloaders, models, optimizers, etc.).
-
-    Finally, it starts the training loop.
-
-    Parameters:
-        config (dict)               : config specs read from a yaml file.
-        device (str)                : selected device to run training on. Either 'cpu' or 'cuda:<N>'.
-        tb_writer (SummaryWriter)   : tensorboard writer.
-        logger (logging.logger)     : python logger object.
-    """
-    # Read and load specifications from config
-    training_dict = prepare_training(config, device, tb_writer, logger)
-
-    
-    pass
