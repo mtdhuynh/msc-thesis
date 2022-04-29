2f37773bed2899e22da327ed941f398bfb259c82
Tam Huynh <tamdavide.huynh@gmail.com>
2022-04-29 13:29:18 +0000

    Implement get_optimizer() helper function

---
+++ src/optimizers/optimizers_utils.py
@@ -0,0 +1,25 @@
+from optimizers import ODOptimizer
+
+def get_optimizer(model, logger=None, **params):
+    """
+    This function returns an optimizer, as specified in the config dict.
+
+    Parameters:
+        model (torch.nn.Module)     : the selected model for object detection.
+        logger (logging.logger)     : python logger object.
+        **params (dict)             : admissible torch.optim kwargs (optimizer configuration parameters - name of optimization algorithm, lr, etc.).
+
+    Returns:
+        optimizer (torch.optim)
+    """
+    # Get the name of the optimizer
+    name = params['name']
+    # Get the rest of the parameters (to use .pop('name') we would need to make a deepcopy)
+    params = {k: v for k,v in params.items() if k!='name'} 
+    
+    optimizer = ODOptimizer(model, name, **params).optimizer
+
+    if logger:
+        logger.info(f'Using optimizer: {optimizer}.')
+
+    return optimizer
