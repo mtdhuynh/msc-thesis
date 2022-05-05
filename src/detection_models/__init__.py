0a776436287be850bce0d0cc1486d22b679946a2
Tam Huynh <tamdavide.huynh@gmail.com>
2022-05-05 15:15:53 +0000

    Add FasterRCNN to model zoo

--- src/detection_models/__init__.py
+++ src/detection_models/__init__.py
@@ -1,12 +1,15 @@
 from detection_models.detection_models import ODModel
 from detection_models.detection_model_utils import build_detection_backbone, get_model
 
+# MODELS
+from detection_models.model_zoo.fasterrcnn import fasterrcnn
 from detection_models.model_zoo.retinanet import retinanet
 from detection_models.model_zoo.yolov3 import yolov3
 from detection_models.model_zoo.yolov5 import yolov5
 
 __all__ = [
     build_detection_backbone,
+    fasterrcnn,
     get_model,
     ODModel,
     retinanet,
---
+++ src/detection_models/model_zoo/fasterrcnn.py
@@ -0,0 +1,38 @@
+import torch
+
+from torchvision.models.detection import FasterRCNN
+
+from detection_models import build_detection_backbone
+
+def fasterrcnn(backbone='resnet50', pretrained=False, num_classes=7, device=torch.device('cpu'), other_kwargs={'trainable_layers': 5}):
+    """
+    Loads a FasterRCNN model with the specified backbone. 
+
+    Parameters:
+        backbone (str)          : the feature extractor. Usually a CNN model of sorts (e.g., ResNet50).
+        pretrained (bool)       : whether to use pretrained weights or not. 
+        num_classes (int)       : number of output classes.
+        device (torch.device)   : device to load the model on.
+        other_kwargs (dict)     : admissible kwargs for backbone, anchor generator, model (pretrained, trainable_layers, etc.).
+
+    Returns:
+        model (torch.nn.Module)
+    """
+    # Separate backbone kwargs from model kwargs
+    backbone_kwargs = {k: v for k,v in other_kwargs.items() if k not in ('img_size')}
+
+    # Get another backbone classifier model and return only its features with the output channels.
+    backbone, anchor_generator = build_detection_backbone(backbone, pretrained=pretrained, **backbone_kwargs)
+
+    # Use default AnchorGenerator and default Head
+    model = FasterRCNN(
+        backbone, 
+        num_classes,
+        rpn_anchor_generator=anchor_generator, 
+        min_size=other_kwargs['img_size'], # do not resize
+        max_size=other_kwargs['img_size'], # do not resize
+        image_mean=(0.0, 0.0, 0.0), # do not normalize
+        image_std=(1.0, 1.0, 1.0) # do not normalize
+    )
+
+    return model.to(device)
